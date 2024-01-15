3f2dbacfd4c86ff2a021c926327015072ed8cc99
DootMario <rautumihai.ionut@gmail.com>
2024-01-15 22:16:12 +0200

    crazy im not spending more time designing this

--- index.php
+++ index.php
@@ -8,10 +8,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Catalog</title>
 </head>
-<body>
-<div class="container bg-primary">
-    <h1 class="justify-content-center">Login</h1>
+<body data-bs-theme="dark">
+<div class="container bg-secondary-subtle my-4 p-4">
+    <h1 class="text-center">Login</h1>
+    <form action="GET" name="login" class="needs-validation" novalidate>
+        <fieldset>
+            <div class="row">
+                <div class="col-12 m-2 p-2">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
+                </div>
+                <div class="col-12 m-2 p-2">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="text" class="form-control" id="password" name="password" required>
+                </div>
+            </div>
+        </fieldset>
+        <button class="btn btn-primary m-2" type="submit">Login</button>
+    </form>
 </div>
+<script src="scripts.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
